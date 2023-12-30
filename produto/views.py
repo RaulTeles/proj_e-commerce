37e3e1b2452d6037479719d2f5b8392d1d07dd16
RaulTeles <telesraul@gmail.com>
2023-12-29 21:12:29 -0300

    Fazendo template dos produtos, configurando as variações

--- produto/views.py
+++ produto/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 from django.views import View
 from django.http import HttpResponse
 from . import models
@@ -11,9 +12,12 @@ class ListaProduto(ListView):
     context_object_name = 'produtos'
     paginate_by = 6
 
-class DetalheProduto(View):
-    def get(self, *args, **kwargs):
-        return HttpResponse('Detalhe Produto')
+class DetalheProduto(DetailView):
+    model = models.Produto
+    template_name = 'produto/detalhe.html'
+    context_object_name = 'produto'
+    slug_url_kwarg = 'slug'
+
 class AddToCard(View):
     def get(self, *args, **kwargs):
         return HttpResponse('Adicionar no carrinho')
